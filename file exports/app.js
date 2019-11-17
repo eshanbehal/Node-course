ac1b388529fe7c45f21be5e3754964e4817e3754
eshan <eshan786behal@gmail.com>
2019-11-17 14:36:28 +0530

    ...

--- file exports/app.js
+++ file exports/app.js
@@ -4,3 +4,8 @@ console.log(module);
 
 const greeting = require('./library/greeting.js');
 greeting();
+
+const average = require('./library/school.js').average;
+console.log(average([87,85,73,75,90.309]));
+
+console.log(require('./library/school.js').grades);
