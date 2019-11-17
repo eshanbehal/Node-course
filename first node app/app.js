0a0db235180d27161b8a8ae2502070b98c05410a
eshan <eshan786behal@gmail.com>
2019-11-17 10:35:40 +0530

    learning node.

---
+++ first node app/app.js
@@ -0,0 +1,9 @@
+console.log('hi');
+console.log('its');
+console.log('eshan here');
+
+//process variable: equivalent of document inside the browser.
+
+// console.log(process);
+
+console.log(global);
