4a4677f8dee9d4e8a3790f830896b2b995cf1189
eshan <eshan786behal@gmail.com>
2019-11-17 10:54:46 +0530

    ...

--- first node app/greet.js
+++ first node app/greet.js
@@ -1,3 +1,6 @@
-const firstname = process.argv[2];
-const lastname = process.argv[3];
+const upper_first = firstname => firstname[0].toUpperCase() + firstname.substring(1);
+let firstname = process.argv[2];
+firstname = upper_first(firstname);
+let lastname = process.argv[3];
+lastname = upper_first(lastname);
 console.log(`Hello ${firstname} ${lastname}!`);
