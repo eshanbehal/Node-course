f8879b55fe31d9eceee60229ba5f00df50db556d
eshan <eshan786behal@gmail.com>
2019-11-18 15:09:13 +0530

    ...

--- fs built in module/app.js
+++ fs built in module/app.js
@@ -2,11 +2,18 @@
 const fs = require('fs');
 const mars = 'mars.txt';
 const venus = 'venus.txt';
+const jupiter = 'jupiter.txt';
 
 
 //check if mars exist:
 console.log(fs.existsSync(mars));
 
 //read mars
-const data = fs.readFileSync(mars, 'utf-8');
+let data = fs.readFileSync(mars, 'utf-8');
 console.log(`${mars} content: ${data}`);
+
+//create jupiter or override content
+fs.writeFileSync(jupiter, 'jupiter has the shortest day of all the planets.');
+//read jupiter
+data = fs.readFileSync(jupiter, 'utf-8');
+console.log(`${jupiter} content: ${data}`);
